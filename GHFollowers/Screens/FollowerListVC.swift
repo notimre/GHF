8f504ddd0fa88cd1995a89fb5aa6afc5a6e98207
Imre Draskovits <i.draskovits@ncl.ac.uk>
2020-10-22 12:42:32 +0100

    Added UICollectionView

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class FollowerListVC: UIViewController {
 
+    enum Section { case main }
+    
     var username: String!
+    var followers: [Follower] = []
+    
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     
     override func viewDidLoad() {
@@ -18,6 +23,7 @@ class FollowerListVC: UIViewController {
         configureViewController()
         configureCollectionView()
         getFollowers()
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,23 +38,43 @@ class FollowerListVC: UIViewController {
     
     func configureCollectionView() {
         // we need to add collectionView first
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         // otherwise collectionView would've been nil as we initalise it a line above
         view.addSubview(collectionView)
         
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
     func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+                                                    // you can put "unowned" ^^^ here, so it force unwraps self, replacing the line below
+            guard let self = self else { return } // this unwraps "self" so we don't need to write ? every time using it
+
             switch result {
                 case .success(let followers):
-                    print(followers)
+                    self.followers = followers
+                    self.updateData()
+                    
                 case .failure(let error):
                     self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
         }
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: {(collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) } // putting it on main thread
     }
 }
---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,25 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Imre Draskovits on 22/10/2020.
+//
+
+import UIKit
+
+struct UIHelper {
+  
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width // total width of the current screen, doesn't matter which one
+        let padding: CGFloat = 12
+        let miniumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (miniumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40) // + 40 adds extra space for the label
+        
+        return flowLayout
+    }
+}

9db2929c70918c6fcb5a1ae13f25090a443e1186
Imre Draskovits <i.draskovits@ncl.ac.uk>
2020-10-25 14:47:58 +0000

    Added user info network call

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -55,4 +55,46 @@ class NetworkManager {
         
         task.resume() // this starts the network call !! don't forget it
     }
+    
+    func getUserInfo(for username: String, completed: @escaping(Result<User, GFError>) -> Void) {
+        let endpoint = baseURL + "\(username)"
+        
+        guard let url = URL(string: endpoint) else {
+            // we can't call the custom alert here, since it'd be called from a background thread which you can't do so we go
+            completed(.failure(.invalidUsername))
+            return
+        }
+        
+        // this is the basic native way to make network calls, you can use anything else to do so too
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let _ = error {
+                completed(.failure(.unableToComplete)) // often happens when no internet
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                // this is where we set snake_case to camelCase
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
+                
+            } catch {
+                //completed(nil, error.localizedDescription) // this is a great way to show developers what's wrong with your app, not suitable for users
+                completed(.failure(.invalidData))
+            }
+        }
+        
+        task.resume() // this starts the network call !! don't forget it
+    }
 }
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -5,4 +5,11 @@
 //  Created by Imre Draskovits on 25/10/2020.
 //
 
-import Foundation
+import UIKit
+           // rawVale
+enum GFError: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}

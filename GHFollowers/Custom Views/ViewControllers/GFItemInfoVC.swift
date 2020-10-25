d8e6465e348a8a1e807142cc0d85b5c39f4d8ac6
Imre Draskovits <i.draskovits@ncl.ac.uk>
2020-10-25 18:12:52 +0000

    Added more details to UserInfo

---
+++ GHFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,17 @@
+//
+//  GFItemInfoVC.swift
+//  GHFollowers
+//
+//  Created by Imre Draskovits on 25/10/2020.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}

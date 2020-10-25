c6ffc71e0e12e72c2752817c38dc08094499d0ba
Imre Draskovits <i.draskovits@ncl.ac.uk>
2020-10-25 17:15:15 +0000

    Added user header info

---
+++ GHFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Imre Draskovits on 25/10/2020.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+
+    private func configure() {
+        textColor = .secondaryLabel // light grey color, if you want to download semantic UI app, useful to showcase these texts!
+        adjustsFontSizeToFitWidth = true // shrinks it if it doesn't fit
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail // puts "..." at the end if doesn't fit
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Imre Draskovits on 25/10/2020.
+//
+
+import UIKit
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}

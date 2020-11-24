d5d55310508d88d5904a647b7098a5e253e4f7d0
Imre Draskovits <i.draskovits@ncl.ac.uk>
2020-11-24 20:04:37 +0000

    Refactored networking, date

--- GHFollowers/Extensions/String+Ext.swift
+++ GHFollowers/Extensions/String+Ext.swift
@@ -4,24 +4,25 @@
 //
 //  Created by Imre Draskovits on 25/10/2020.
 //
-
-import UIKit
-
+////  This file is not needed, I left it in to reference it later
+//
+//import UIKit
+//
 // nsdateformatter.com
-
-extension String {
-    
-    func convertToDate() -> Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        dateFormatter.locale = Locale(identifier: "en_GB")
-        dateFormatter.timeZone = .current
-        
-        return dateFormatter.date(from: self)
-    }
-    
-    func convertToDisplayFormat() -> String {
-        guard let date = self.convertToDate() else { return "Member since Unkown" }
-        return date.convertToMonthYearFormat()
-    }
-}
+//
+//extension String {
+//
+//    func convertToDate() -> Date? {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+//        dateFormatter.locale = Locale(identifier: "en_GB")
+//        dateFormatter.timeZone = .current
+//
+//        return dateFormatter.date(from: self)
+//    }
+//
+//    func convertToDisplayFormat() -> String {
+//        guard let date = self.convertToDate() else { return "Member since Unkown" }
+//        return date.convertToMonthYearFormat()
+//    }
+//}
--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -86,6 +86,7 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 // this is where we set snake_case to camelCase
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
+                decoder.dateDecodingStrategy = .iso8601 // this is the standard
                 let user = try decoder.decode(User.self, from: data)
                 completed(.success(user))
                 
@@ -96,5 +97,35 @@ class NetworkManager {
         }
         
         task.resume() // this starts the network call !! don't forget it
+    }
+
+    func downloadImage(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+        
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self,
+                  error == nil,
+                  let response = response as? HTTPURLResponse, response.statusCode == 200,
+                  let data = data,
+                  let image = UIImage(data: data) else {
+                    completed(nil)
+                    return
+                  }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image) // from line 120
+        }
+        
+        task.resume()
     }
 }
